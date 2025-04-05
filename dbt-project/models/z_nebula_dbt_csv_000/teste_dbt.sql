84ed4b53968f16bda7b1c54f976ce37db1b2610b
Johnnathan <johnnypaixao@hotmail.com>
2025-04-05 17:48:28 +0100

    dbt-test

--- dbt-project/models/z_nebula_dbt_csv_000/teste_dbt.sql
+++ dbt-project/models/z_nebula_dbt_csv_000/teste_dbt.sql
@@ -1,6 +1,3 @@
-with dados as (
-  select 'Alice' as nome, 30 as idade
-  union all
-  select 'Bruno' as nome, 25 as idade
-)
-select * from dados
+select 'Alice' as nome, 30 as idade
+union all
+select 'Bruno' as nome, 25 as idade
