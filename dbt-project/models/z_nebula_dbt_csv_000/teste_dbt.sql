b909fe5c6b415279575a4b879d7da2f4e981ed7e
Johnnathan <johnnypaixao@hotmail.com>
2025-04-05 18:12:27 +0100

    dbt-test

--- dbt-project/models/z_nebula_dbt_csv_000/teste_dbt.sql
+++ dbt-project/models/z_nebula_dbt_csv_000/teste_dbt.sql
@@ -1,4 +1,6 @@
-{{ config(materialized='incremental') }}
+-- models/teste_dbt.sql
+
+{{ config(materialized='table', schema='z_nebula_dbt_csv_000') }}
 
 select 'Alice' as nome, 30 as idade
 union all
